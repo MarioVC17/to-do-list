37de624679da855b74bfaa96ed82a108009614fd
Mario <mario2002velasco@gmail.com>
2025-04-06 15:54:43 -0500

    docs(task-list): añadida documentación detallada del componente y renombrados métodos para mayor claridad

--- src/app/pages/task-list/task-list.component.ts
+++ src/app/pages/task-list/task-list.component.ts
@@ -24,7 +24,6 @@ import { CategoriesComponent } from 'src/app/components/modals/categories/catego
     IonList,
     IonCheckbox,
     ReactiveFormsModule,
-    IonPopover,
     IonListHeader,
     CommonModule,
     IonButton,
@@ -39,52 +38,105 @@ import { CategoriesComponent } from 'src/app/components/modals/categories/catego
 
 export class TaskListComponent  implements OnInit {
 
+  /**
+   * @property taskList
+   * @type {Array<{ name: string; status: boolean, categorie: string }>}
+   * @description-  Array que almacena todas las tareas. Cada tarea tiene un nombre, estado (completada o no) y categoría.
+   */
   public taskList: Array<{ name: string; status: boolean, categorie: string }> = [];
 
-  public selectedFilterCategory: string = '';
-
+  /**
+   * @property filteredTaskList
+   * @type {Array<{ name: string; status: boolean, categorie: string }>}
+   * @description Array que contiene la lista de tareas después de aplicar el filtro por categoría.
+   */
   public filteredTaskList: Array<{ name: string; status: boolean, categorie: string }> = [];
 
+  /**
+   * @property categoryList
+   * @type {Array<string>}
+   * @description Array que contiene la lista de categorías disponibles para las tareas. Inicializada con valores por defecto.
+   */
   public categoryList: Array<string> = ['Urgente', 'Normal', 'No urgente'];
 
+  /**
+   * @property selectedFilterCategory
+   * @type {string}
+   * @description Almacena la categoría seleccionada para filtrar la lista de tareas. Un valor vacío indica que no hay filtro aplicado.
+   */
+  public selectedFilterCategory: string = '';
+
+  /**
+   * @constructor
+   * @param {ModalController} _modalCtrl 
+   * @param {StorageService} _storageService 
+   */
   constructor(
-    private _storageService: StorageService,
-    private _modalCtrl: ModalController
+    private _modalCtrl: ModalController,
+    private _storageService: StorageService
   ) {
     addIcons({
       add,
       trash,
+      pencilOutline,
+      createOutline,
       pricetagOutline,
       returnUpBackOutline,
-      createOutline,
-      pencilOutline
     });
   }
 
+  /**
+   * @OnInit
+   * @description Hook del ciclo de vida que se ejecuta una vez que el componente ha sido inicializado, en este
+   * se carga las tareas y las categorías desde el almacenamiento local y aplica el filtro inicial.
+   */
   async ngOnInit() {
-    await this.loadTasks();
-    await this.loadCategories();
-    this.filterTasks();
+    await this.loadTasksFromStorage();
+    await this.loadCategoriesFromStorage();
+    this.applyCategoryFilter();
   }
 
+  /**
+   * @method pendingTasks
+   * @returns {Array<{ name: string; status: boolean, categorie: string }>}
+   * @description Getter que devuelve la lista de tareas pendientes (aquellas dónde la propiedad 'status' es false).
+   */
   public get pendingTasks(): Array<{ name: string; status: boolean, categorie: string }> {
     return this.filteredTaskList.filter(task => !task.status);
   }
 
+  /**
+   * @method completedTasks
+   * @returns {Array<{ name: string; status: boolean, categorie: string }>}
+   * @description Getter que devuelve la lista de tareas completadas (aquellas dónde la propiedad 'status' es true).
+   */
   public get completedTasks(): Array<{ name: string; status: boolean, categorie: string }> {
     return this.filteredTaskList.filter(task => task.status);
   }
 
-  public filterTasks(category?: string) {
-    this.selectedFilterCategory = category !== undefined ? category : this.selectedFilterCategory;
+  /**
+   * @method applyCategoryFilter 
+   * @param {string} categorie - La categoría por la cual filtrar. Si no se proporciona, se utiliza la 'selectedFilterCategory' actual.
+   * @description Filtra la 'taskList' y actualiza la 'filteredTaskList' basándose en la categoría seleccionada.
+   * Si no hay una categoría seleccionada, se muestran todas las tareas.
+   */
+  public applyCategoryFilter(categorie?: string) {
+    this.selectedFilterCategory = categorie !== undefined ? categorie : this.selectedFilterCategory;
     if (!this.selectedFilterCategory) {
-      this.filteredTaskList = [...this.taskList]; // Mostrar todas las tareas si no hay filtro seleccionado
+      this.filteredTaskList = [...this.taskList];
     } else {
       this.filteredTaskList = this.taskList.filter(task => task.categorie === this.selectedFilterCategory);
     }
   }
 
-  public async  addTask() {
+  /**
+   * @method openAddTaskModal
+   * @async
+   * @returns {Promise<void>}
+   * @description Abre un modal para agregar una nueva tarea. Pasa la lista de categorías al modal.
+   * Después de que el modal se cierra con éxito ('saved'), recarga la lista de tareas.
+   */
+  public async openAddTaskModal(): Promise<void> {
     const modal = await this._modalCtrl.create({
       component: AddTaskComponent,
       componentProps: {
@@ -94,21 +146,42 @@ export class TaskListComponent  implements OnInit {
     modal.present();
     const { role } = await modal.onDidDismiss();
     if (role === 'saved') {
-      await this.loadTasks();
+      await this.loadTasksFromStorage();
     }
   }
 
-  public async removeTask(index: number) {
+  /**
+   * @method removeTask
+   * @async
+   * @param {number} index Número del índice de la tarea a eliminar en la 'taskList'.
+   * @returns {Promise<void>}
+   * @description Elimina una tarea de la 'taskList' en el índice especificado y guarda la lista actualizada en el almacenamiento local.
+   */
+  public async removeTask(index: number): Promise<void> {
     this.taskList.splice(index, 1);
-    await this.saveTask();
+    await this.saveTasksToStorage();
   }
 
-  public async updateTaskStatus(task: any) {
+  /**
+   * @method toggleTaskStatus
+   * @async
+   * @param {any} task El objeto de la tarea dónde el estado se va a cambiar.
+   * @returns {Promise<void>}
+   * @description Cambia el estado de completado ('status') de una tarea y guarda la lista actualizada en el almacenamiento local
+   */
+  public async toggleTaskStatus(task: any): Promise<void> {
     task.status = !task.status;
-    await this.saveTask();
+    await this.saveTasksToStorage();
   }
 
-  public async openCategoriesModal() {
+  /**
+   * @method openCategoriesModal
+   * @async
+   * @returns {Promise<void>}
+   * @description Abre un modal para gestionar las categorías, si el modal se cierra con éxito ('saved') y devuelve 
+   * una lista de categorías actualizada, actualiza la 'categoryList' local y la guarda en el almacenamiento.
+   */
+  public async openCategoriesModal(): Promise<void> {
     const modal = await this._modalCtrl.create({
       component: CategoriesComponent,
       componentProps: {
@@ -118,26 +191,55 @@ export class TaskListComponent  implements OnInit {
     modal.present();
   }
 
-  private async loadTasks() {
+  /**
+   * @private
+   * @method loadTasksFromStorage
+   * @async
+   * @returns {Promise<void>}
+   * @description Carga la lista de tareas desde el almacenamiento local y actualiza la 'taskList'.
+   */
+  private async loadTasksFromStorage(): Promise<void> {
     const storedTasks = await this._storageService.getValue('tasks');
     this.taskList = storedTasks ? JSON.parse(storedTasks) : [];
-    this.filterTasks();
+    this.applyCategoryFilter();
   }
 
-  private async saveTask() {
+  /**
+   * @private
+   * @method saveTasksToStorage
+   * @async
+   * @returns {Promise<void>}
+   * @description Guarda la 'taskList' actual en el almacenamiento local.
+   */
+  private async saveTasksToStorage(): Promise<void> {
     await this._storageService.setValue('tasks', this.taskList);
   }
 
-  private async loadCategories() {
+  /**
+   * @private
+   * @method loadCategoriesFromStorage
+   * @async
+   * @returns {Promise<void>}
+   * @description Carga la lista de categorías desde el almacenamiento local y actualiza la 'categoryList', en caso de
+   * que la lista este vacía, guarda la lista por defecto.
+   */
+  private async loadCategoriesFromStorage(): Promise<void> {
     const storedCategories = await this._storageService.getValue('categories');
     if (storedCategories) {
       this.categoryList = JSON.parse(storedCategories);
     } else {
-      await this.saveCategories(); // Guarda la lista por defecto si no existe
+      await this.saveCategoriesToStorage();
     }
   }
 
-  private async saveCategories() {
+  /**
+   * @private
+   * @method saveCategoriesToStorage
+   * @async
+   * @returns {Promise<void>}
+   * @description uarda la 'categoryList' actual en el almacenamiento local.
+   */
+  private async saveCategoriesToStorage(): Promise<void> {
     await this._storageService.setValue('categories', this.categoryList);
   }
 }
